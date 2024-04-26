6cfb76c9c4cbbf9d72ebf46ca8b329e5842fe7f9
Dawid Iwanicki <iwanickidawid@gmail.com>
2024-04-26 17:07:23 +0200

    Done task 3

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,12 @@
-const x = 10;
+const x = 5;
 let iteration = 0;
 let randomNumber = -1;
+
+
+while(randomNumber !== x) {
+  randomNumber = Math.round(Math.random() * 30);
+  iteration++;
+  console.log(randomNumber, iteration);
+}
+
+alert('Wylosowano ' + x + ' za ' + iteration + ' razem');
